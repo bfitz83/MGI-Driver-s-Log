3cab7b66f65384323ead9966fb3817f65427db88
bfitz83 <115970022+bfitz83@users.noreply.github.com>
2023-03-13 18:00:38 -0400

    Got the login to work

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-import { loginInfo } from "./logInCredentials.js"
-import { storeData } from "./storeData.js"
+import { loginInfo } from "./logInCredentials.js" // stores login for now. Will be a SQL database later
+import { storeData } from "./storeData.js" // Just the store information
 
-console.log(loginInfo)
-console.log(storeData)
+// console.log(loginInfo[0].name, loginInfo[0].passWord)
+
+
+let driverName = document.getElementById("driver-name")
+let driverPassword = document.getElementById("driver-password")
+const storeName = document.getElementById("store-name")
+
+// this function makes the store list for the driver to select. As this project progresses 
+//      there will be a seperate login page so that more store information 
+//      can be added without changing the code.
+
+    function renderStoreList(){
+        // you need a for each to make this better
+        for (let i = 0; i < storeData.length; i++){
+            let storeList = document.createElement("option")
+            storeList.innerText = `${storeData[i].storeNumber} ${storeData[i].storeName}`
+            storeName.appendChild(storeList)
+        }
+    }
+
+    renderStoreList()
 
 document.addEventListener("click", function(e){
-    if (e.target.id === "submit-store-info"){
+    if (e.target.id === "submit-driver-login") {
+        // This is a basic login to be replaced with a SQL database
+        //      maybe use filter() ?
+        if (driverName.value === loginInfo[0].name && driverPassword.value === loginInfo[0].passWord){
+        document.getElementById("driver-login").style.display = "none"
+        document.getElementById("departure-info-section").style.display = "flex"
+        // change the driver's logged in info in logincredentials
+        // add a pop up that diplays any road problems
+    }
+    } else if (e.target.id === "submit-store-info"){
         document.getElementById("departure-varification").style.display = "flex"
     } else if (e.target.id === "submit-id-verification") {
         document.getElementById("depart-time").style.display = "flex"
--- logInCredentials.js
+++ logInCredentials.js
@@ -1,6 +1,10 @@
 export const loginInfo = [
     {
         name: "Brian",
-        passWord: "123abc"
+        passWord: "123abc",
+        isLogedIn: false
     }
 ]
+
+// the isLogedIn will deal with the driver's route info 
+// add gaurd info
